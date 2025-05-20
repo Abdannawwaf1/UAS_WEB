43f59407d087a52af268593f36369bc854ab79fb
Abdannawwaf1 <abdannawwaf1@gmail.com>
2025-05-20 11:39:04 +0700

    update database.sql

--- db_qurban.sql
+++ db_qurban.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: May 20, 2025 at 06:29 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Host: localhost:3306
+-- Generation Time: May 20, 2025 at 04:38 AM
+-- Server version: 9.0.0
+-- PHP Version: 8.3.13
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,9 +28,9 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `detail_qurban` (
-  `id_detail_qurban` int(11) NOT NULL,
-  `id_qurban` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL
+  `id_detail_qurban` int NOT NULL,
+  `id_qurban` int NOT NULL,
+  `id_user` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -40,9 +40,9 @@ CREATE TABLE `detail_qurban` (
 --
 
 CREATE TABLE `hewan` (
-  `id_hewan` int(11) NOT NULL,
-  `nama_hewan` varchar(20) NOT NULL,
-  `harga` int(11) NOT NULL
+  `id_hewan` int NOT NULL,
+  `nama_hewan` varchar(20) COLLATE utf8mb4_general_ci NOT NULL,
+  `harga` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -52,11 +52,11 @@ CREATE TABLE `hewan` (
 --
 
 CREATE TABLE `pengambilan_daging` (
-  `id_pengambilan` int(11) NOT NULL,
-  `id_user` int(11) NOT NULL,
+  `id_pengambilan` int NOT NULL,
+  `id_user` int NOT NULL,
   `jumlah_daging` decimal(5,2) NOT NULL,
-  `status` enum('Belum Diambil','Sudah Diambil','','') NOT NULL,
-  `qrcode_token` varchar(50) NOT NULL
+  `status` enum('Belum Diambil','Sudah Diambil','') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `qrcode_token` varchar(50) COLLATE utf8mb4_general_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -66,9 +66,9 @@ CREATE TABLE `pengambilan_daging` (
 --
 
 CREATE TABLE `qurban` (
-  `id_qurban` int(11) NOT NULL,
-  `id_hewan` int(11) NOT NULL,
-  `biaya_total` int(11) NOT NULL
+  `id_qurban` int NOT NULL,
+  `id_hewan` int NOT NULL,
+  `biaya_total` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -78,12 +78,12 @@ CREATE TABLE `qurban` (
 --
 
 CREATE TABLE `transaksi_keuangan` (
-  `id_transaksi` int(11) NOT NULL,
+  `id_transaksi` int NOT NULL,
   `tanggal` date NOT NULL,
-  `keterangan` varchar(255) NOT NULL,
-  `tipe` enum('masuk','keluar') NOT NULL,
-  `jumlah` int(11) DEFAULT NULL,
-  `id_qurban` int(11) DEFAULT NULL
+  `keterangan` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `tipe` enum('masuk','keluar') COLLATE utf8mb4_general_ci NOT NULL,
+  `jumlah` int DEFAULT NULL,
+  `id_qurban` int DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -93,12 +93,12 @@ CREATE TABLE `transaksi_keuangan` (
 --
 
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
-  `nama` varchar(50) NOT NULL,
-  `username` varchar(15) NOT NULL,
-  `password` varchar(15) NOT NULL,
-  `is_panitia` tinyint(4) NOT NULL,
-  `is_berqurban` tinyint(4) NOT NULL
+  `id_user` int NOT NULL,
+  `nama` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `username` varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(15) COLLATE utf8mb4_general_ci NOT NULL,
+  `is_panitia` tinyint NOT NULL,
+  `is_berqurban` tinyint NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -109,7 +109,9 @@ CREATE TABLE `user` (
 -- Indexes for table `detail_qurban`
 --
 ALTER TABLE `detail_qurban`
-  ADD PRIMARY KEY (`id_detail_qurban`);
+  ADD PRIMARY KEY (`id_detail_qurban`),
+  ADD KEY `id_qurban` (`id_qurban`),
+  ADD KEY `id_user` (`id_user`);
 
 --
 -- Indexes for table `hewan`
@@ -121,13 +123,15 @@ ALTER TABLE `hewan`
 -- Indexes for table `pengambilan_daging`
 --
 ALTER TABLE `pengambilan_daging`
-  ADD PRIMARY KEY (`id_pengambilan`);
+  ADD PRIMARY KEY (`id_pengambilan`),
+  ADD KEY `id_user` (`id_user`);
 
 --
 -- Indexes for table `qurban`
 --
 ALTER TABLE `qurban`
-  ADD PRIMARY KEY (`id_qurban`);
+  ADD PRIMARY KEY (`id_qurban`),
+  ADD KEY `id_hewan` (`id_hewan`);
 
 --
 -- Indexes for table `transaksi_keuangan`
@@ -140,7 +144,7 @@ ALTER TABLE `transaksi_keuangan`
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`id_user`) USING BTREE,
   ADD UNIQUE KEY `username` (`username`);
 
 --
@@ -151,41 +155,60 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `detail_qurban`
 --
 ALTER TABLE `detail_qurban`
-  MODIFY `id_detail_qurban` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_detail_qurban` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `hewan`
 --
 ALTER TABLE `hewan`
-  MODIFY `id_hewan` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_hewan` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `pengambilan_daging`
 --
 ALTER TABLE `pengambilan_daging`
-  MODIFY `id_pengambilan` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_pengambilan` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `qurban`
 --
 ALTER TABLE `qurban`
-  MODIFY `id_qurban` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_qurban` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `transaksi_keuangan`
 --
 ALTER TABLE `transaksi_keuangan`
-  MODIFY `id_transaksi` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_transaksi` int NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_user` int NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `detail_qurban`
+--
+ALTER TABLE `detail_qurban`
+  ADD CONSTRAINT `FK_detail_qurban_qurban` FOREIGN KEY (`id_qurban`) REFERENCES `qurban` (`id_qurban`),
+  ADD CONSTRAINT `FK_detail_qurban_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+
+--
+-- Constraints for table `pengambilan_daging`
+--
+ALTER TABLE `pengambilan_daging`
+  ADD CONSTRAINT `FK_pengambilan_daging_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
+
+--
+-- Constraints for table `qurban`
+--
+ALTER TABLE `qurban`
+  ADD CONSTRAINT `FK_qurban_hewan` FOREIGN KEY (`id_hewan`) REFERENCES `hewan` (`id_hewan`);
 
 --
 -- Constraints for table `transaksi_keuangan`
